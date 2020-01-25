fc55b028effb0feecf94467beb0533beda0028c5
jp1995 <johanpoldaru@gmail.com>
2020-01-25 12:54:30 +0200

    kapp

--- .config/polybar/cputemp.sh
+++ .config/polybar/cputemp.sh
@@ -3,5 +3,6 @@
 
 tempGPU=$(sensors | grep -i edge: | head -n1 | sed -r 's/.*:\s+[\+-]?(.*C)\s+.*/\1/')
 temp=$(sensors | grep -i AUXTIN0 | head -n1 | sed -r 's/.*:\s+[\+-]?(.*C)\s+.*/\1/')
+#fan=$(sensors | grep -i fan1 | head -n1 | sed -r 's/.*:\s+[\+-]?(.*RPM)\s+.*/\1/')
 
-printf " CPU: $temp / GPU: $tempGPU"
+printf " CPU: $temp / GPU: $tempGPU $fan"
