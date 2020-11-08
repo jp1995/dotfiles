e386aabb6bc112daac5453bc8dee8b51cbb64a5f
jp1995 <johanpoldaru@gmail.com>
2020-11-08 14:04:33 +0200

    november update

--- .config/polybar/pkg.sh
+++ .config/polybar/pkg.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+if [[ "ping -q -w 2 -c 1 176.34.135.167" > /dev/null ]]; then
+  pac=$(checkupdates | wc -l)
+  aur=$(yay -Qum 2> /dev/null | wc -l)
 
-pac=$(checkupdates | wc -l)
-aur=$(yay -Qum 2> /dev/null | wc -l)
-
-check=$(($pac + $aur))
-if [[ "$check" != "0" ]]
-then
-    echo "$pac %{F#25232A}%{F-} $aur"
+  check=$(($pac + $aur))
+  if [[ "$check" != "0" ]]
+  then
+      echo "$pac %{F#25232A}%{F-} $aur"
+  else
+  	echo "%{F#25232A}%{F-} No updates!"
+  fi
 else
-	echo "%{F#25232A}%{F-} No updates!"
+  echo "no interwebs"
 fi
