aaa9235286251d303bce78b23eb165b752a2b231
jp1995 <johanpoldaru@gmail.com>
2021-01-20 01:04:32 +0200

    january update

--- scripts/sharenix.sh
+++ scripts/sharenix.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sharenix-section -upload=false
-i3-msg "workspace number 1" &&
-exec dolphin /home/johan/sharenix/archive &
-exec /home/johan/i3lm/layout_manager.sh DOLPHIN
+import png:- | xclip -selection clipboard -t image/png
+sleep 1
+xclip -selection clipboard -o > /home/johan/Pictures/screenshots/`date +%Y-%m-%d_%T`.png
